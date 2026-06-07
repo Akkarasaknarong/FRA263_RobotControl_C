116c914d56ac648554e825617b600bc3c722811e
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2026-06-08 00:17:16 +0700

    Complete 180 - 215 Degree

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -72,7 +72,7 @@ refTarget_t REFdata = { 0 };
 QEIstruct_t QEIdata = { 0 };
 SSErrorstruct_t SSErrordata = {0};
 KALMANstruct_t ESTdata = { 0 };
-PIDParam_t PIDparam = { .kp_pos = 800,  .ki_pos = 50, .kd_pos = 0, .kp_vel = 400.0f, .ki_vel = 200.0f, .kd_vel =0 };
+PIDParam_t PIDparam = { .kp_pos = 800,  .ki_pos = 50, .kd_pos = 0, .kp_vel = 400.0f, .ki_vel = 1000.0f, .kd_vel =0 };
 SerialFrame_t STLINK_UART_frame;
 
 Robot_t Robot = {
@@ -894,7 +894,7 @@ void Quintic_List(int selec) {
 	float t_slow = 3.25f;
 	float t_fast = 3.25f;
 	float t_break = 2.0f ;
-	float tar_q = 185.0f;
+	float tar_q = 220.0f;
 
 	// Rotate
 	if (selec == 1){
